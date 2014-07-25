16e1fce0f72d85218c001a753f6fe4ec4932f545
JessicaBarnett <BarnettEffect@gmail.com>
2014-07-25 14:02:05 -0400

    trying to adjust media queries so landscape view will work on my android.  committing for testing purposes

--- javascript/controller.js
+++ javascript/controller.js
@@ -1,8 +1,8 @@
 //Media Queries
 
-var mqTab = window.matchMedia("(max-width: 930px)");
+var mqTab = window.matchMedia("(max-width: 1000px)");
 var mqPhone = window.matchMedia("screen and (min-width: 200px) and (max-width: 550px)");
-var mqPhoneWide = window.matchMedia("screen and (min-width: 200px) and (max-width: 550px) and (orientation:landscape)");
+var mqPhoneWide = window.matchMedia("screen and (min-width: 300px) and (max-width: 800px) and (orientation: landscape)");
 
 
 //********* CONTROLLER **********/
