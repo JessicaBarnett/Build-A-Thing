6e5b4aec4b729ca3ea3766a880053f33c8bd690b
JessicaBarnett <BarnettEffect@gmail.com>
2014-08-03 00:35:31 -0400

    fixed a bug.  Make button handler was calling petName instead of humanName in its makeAnyThing call, resulting in pets not getting mixed into pet

--- javascript/controller.js
+++ javascript/controller.js
@@ -382,9 +382,9 @@ ThingForm.prototype.makeButtonHandler = function() {
             
     }
 
-    console.log(this.thingData["type"] + " ***** " + this.thingData["petName"]  + " ***** " +  thingArgs);
+    console.log(this.thingData["type"] + " ***** " + this.thingData["humanName"]  + " ***** " +  thingArgs);
     //makes new Thing with thingArgs array and adds it to the model
-    var newThing = thingModel.makeAnyThing(this.thingData["type"], this.thingData["petName"], thingArgs);
+    var newThing = thingModel.makeAnyThing(this.thingData["type"], this.thingData["humanName"], thingArgs);
     thingModel.addThing(newThing);
 
     //removes "active" class from makeNewThing Button/makes new select button for the new Thing
